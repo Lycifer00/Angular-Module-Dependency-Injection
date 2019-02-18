e671c19ffb74d7dee081b873b74651b0001c17d2
Lycifer00 <Lycifer00@users.noreply.github.com>
2019-02-18 15:48:33 +0700

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgSliderModule } from './img-slider/img-slider.module';
+import { ImageGalleryModule } from './image-gallery/image-gallery.module';
+import { GalleryConfig } from './image-gallery/token';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ImgSliderModule,
+    ImageGalleryModule,
   ],
-  providers: [],
+  // providers: [AuthService],
+  providers: [
+    {provide: GalleryConfig, useValue: 2}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
